9c339f7192468a60dd7ae0e0df3f7f4982cf4eb2
Snigdho Saha <snigdhosaha3@gmail.com>
2024-12-18 00:14:14 +0630

    Add files via upload

---
+++ PHP Tutorial/2comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Outputs a Hare Krishna 
+echo "Hare Krishna";
+/*
+The next statement will
+print a Jay Shri Ram
+*/
+echo "Jay Shri Ram";
+?>
+<br>
+ <p>Using comments to ignore parts of a code line:</p>
+ 
+ <?php
+ $x = 5 /* + 15 */ + 5;
+ echo $x;
+ ?>
+</body>
+</html>
